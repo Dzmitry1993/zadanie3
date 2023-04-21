4ddbc01f8fd55fa2925a900fdf700c002514a26f
Dzmitry <dmitrypetrachkov93@gmail.com>
2023-04-22 01:52:00 +0300

    zadacha3

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ A (7,-5, 0); B (1,-1,9) -> 11.53
 
 */
 
-
+/*
 int x1 = Coordinate("x", "A");
 int y1 = Coordinate("y", "A");
 int z1 = Coordinate("z", "A");
@@ -60,3 +60,36 @@ double Decision(double x1, double x2,
 double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
 
 Console.WriteLine($"Длина отрезка  {segmentLength}");
+*/
+
+/*
+Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+ 3 -> 1, 8, 27
+ 5 -> 1, 8, 27, 64, 125
+ */
+
+ 
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube){
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length){
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
